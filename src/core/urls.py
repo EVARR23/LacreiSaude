06db5fc0052be3e0e550c52cad203e42781fd4fc
EvaReboucas30 <140523579+EvaReboucas30@users.noreply.github.com>
2025-08-07 14:40:47 -0300

    adicionado swagger

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,6 +1,7 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -8,7 +9,15 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # OpenAPI 3
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    #Autenticação
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    #Outros endpoints
     path('api/', include('api.urls')),
 ]
+
+
