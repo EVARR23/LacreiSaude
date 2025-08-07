49d39a819aa2f314eb612acc23cc7639147dbc13
EvaReboucas30 <140523579+EvaReboucas30@users.noreply.github.com>
2025-08-07 18:50:48 -0300

    cors

--- src/core/settings.py
+++ src/core/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'drf_spectacular',
+    'corsheaders',  # Adicione o CORS Headers
     'api',
     
     
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -322,3 +324,5 @@ SPECTACULAR_SETTINGS = {
     
     # OTHER SETTINGS
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
